6910cb4012bce3ecf94c44b8b73c25134f936da7
Ángel Guerrero <lasantiagoguerrero@gmail.com>
2017-11-29 20:19:57 -0600

    Agregando comentarios al archivo de notas

--- Notes.sql
+++ Notes.sql
@@ -1,3 +1,5 @@
-select text from user_source where type = 'FUNCTION' ORDER BY line;
+-- Muestra la descripción de la función
+SELECT text FROM user_source WHERE TYPE = 'FUNCTION' ORDER BY line;
 
+-- Muestra el nombre de las funciones
 SELECT object_name FROM user_objects WHERE object_type = 'FUNCTION';
