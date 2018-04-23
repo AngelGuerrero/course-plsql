f7f0b6c7f417ac9e7ea7d18a649694c1b10ab22d
Angel Guerrero <_angelguerrero_@outlook.com>
2018-04-23 15:03:27 -0500

    Agregando archivos de la práctica 9

---
+++ Practice_9.sql
@@ -0,0 +1,54 @@
+-- Practice 9.
+
+-- 1. Create a table called PERSONNEL by executing the script file,
+-- The table contains the following attributs and data types.
+
+EXECUTE make_table('PERSONNEL', 'last_name VARCHAR2(35), picture BLOB', TRUE);
+
+SELECT *
+  FROM PERSONNEL;
+
+DESC PERSONNEL;
+
+SELECT *
+  FROM PERSONNEL;
+
+INSERT
+  INTO PERSONNEL
+VALUES (2034, 'Allen', EMPTY_CLOB(), NULL);
+
+INSERT
+  INTO PERSONNEL
+VALUES (2035, 'Bond', EMPTY_CLOB(), NULL);
+
+
+
+DECLARE
+   l_text VARCHAR2(4001);
+   l_person PERSONNEL.rowtype;
+BEGIN
+--   SELECT 
+--   RESUME INTO l_text
+--     FROM PERSONNEL
+--    WHERE personnel_id = 1;
+    
+    SELECT
+         * INTO l_person
+      FROM PERSONNEL
+     WHERE personnel_id = 1;
+     
+    dbms_output.put_line('Text is: ' || dbms_lob.getlength(l_person.review));
+END;
+/
+
+SELECT personnel_id,
+       resume -- CLOB
+  FROM PERSONNEL
+ WHERE personnel_id = 1;
+
+
+
+
+
+
+
